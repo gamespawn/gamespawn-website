274b178100d0b74f43935236e15c3e10d6873350
Raidah Fairooz <rfair008@ucr.edu>
2024-04-02 02:33:12 -0700

    landing initial

---
+++ src/components/home/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+import landingleft from "@/public/assets/home/landingLeft.png";
+import landingright from "@/public/assets/home/landingRight.png";
+const Landing = () => {
+  return (
+    <div className="flex justify-evenly items-center">
+      <Image src={landingleft} />
+      Landing
+      <Image src={landingright} />
+    </div>
+  );
+};
+
+export default Landing;
