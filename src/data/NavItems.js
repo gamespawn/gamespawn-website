774f0f9847b46971578d374220b2a200dce0ab17
Rishi-Dave <rishipdave@gmail.com>
2024-04-02 23:47:35 -0700

    added all navbar items and what they link to when clicked

--- src/data/NavItems.js
+++ src/data/NavItems.js
@@ -1,6 +1,26 @@
 export const items = [
   {
-    name: "",
-    link: "/",
+    name: "home",
+    link: "/src/app/home/page.js",
+  },
+  {
+    name: "board",
+    link: "/src/app/board/page.js",
+  },
+  {
+    name: "events",
+    link: "src/app/events/page.js",
+  },
+  {
+    name: "projects",
+    link: "src/app/projects/page.js",
+  },
+  {
+    name: "assets",
+    link: "https://drive.google.com/drive/u/1/folders/1CORrAKgDflCeiEvgjaC63eKB_J64dpC0",
+  },
+  {
+    name: "join-now",
+    link: "https://discord.com/invite/ejzg2Wb",
   },
 ];
