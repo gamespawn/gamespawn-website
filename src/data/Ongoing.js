227aed291f8bb2f0880cb8ae83ee3e80e2d43197
jaymehta002 <jaymehta002@gmail.com>
2024-04-08 12:04:55 +0530

    ongoing.js

--- src/data/Ongoing.js
+++ src/data/Ongoing.js
@@ -1,11 +1,29 @@
-import catImage from "/public/assets/cat.png";
-export const Ongoing = [
-  {
-    name: "Cross Campus",
-    summary:
-      "Go to the University of California and meet the lovely and dateable mascots across the UC's, take and skip classes to spend time with them, and confess and go to college.",
-    image: catImage,
-  },
-];
+import cross from '/public/assets/projects/ongoing/cross.png';
+import ragdoll from '/public/assets/projects/ongoing/ragdoll.png';
+import Other from '/public/assets/projects/ongoing/Other.png';
+import split from '/public/assets/projects/ongoing/split.png';
 
-export default Ongoing;
+export const ONGOING = [
+    {
+        image: cross,
+        title: "Cross Campus",
+        description: "Go to the University of California and meet the lovely and dateable mascots across the UC's, take and skip classes to spend time with them, and confess and go to college." 
+    },
+    {
+        image: ragdoll,
+        title: "Ragdoll",
+        description: "A little cat in a weird world." 
+    },
+    {
+        image: Other,
+        title: "There is no Other side",
+        description: "Psychological Horror Game with an incosistent narrator." 
+    },
+    {
+        image: split,
+        title: "Lickety Split",
+        description: "Lickety Split is an RGP set in te 2000s inn a little place called Old Town the believes it's actually the 1700s. This town faces the threat of to make way of a parking ot of Water Works, an evil company that specialized in making water baloons. With the approval of and Old Geezer, a revolver in one hand, and a water gun in the other, Bee sets out to save the town." 
+    }
+]
+
+export default ONGOING;
