a0f54afa6fe298bb486e9986a6bdc9b5d52702dc
Derrick-Mao <derrickmao03@gmail.com>
2024-04-08 21:12:57 -0700

    fixed blue color, updated description width

--- src/components/events/Card.js
+++ src/components/events/Card.js
@@ -1,7 +1,7 @@
 const Event = ({ month, day, hour, name, location, description }) => {
   return (
-    <div className="flex flex-row justify-center drop-shadow-2xl mx-auto w-2/3">
-      <div className="flex flex-col justify-center bg-blue-100 rounded-l-2xl text-[#ffffff] py-4 w-1/5">
+    <div className="flex flex-row justify-center drop-shadow-2xl mx-auto w-7/12">
+      <div className="flex flex-col justify-center bg-game-blue-100 rounded-l-2xl text-[#ffffff] py-4 w-1/5">
         <div className="font-russo mt-3 text-center md:text-5xl">{month}</div>
         <div className="font-russo text-center md:text-5xl pt-2">{day}</div>
         <div className="font-russo text-center md:text-3xl py-2">{hour}</div>
@@ -11,7 +11,7 @@ const Event = ({ month, day, hour, name, location, description }) => {
         <div className="font-sans ml-5 flex text-left mt-3 text-xl">
           {location}
         </div>
-        <div className="font-sans ml-5 mb-2 mt-3 mr-2 text-lg w-3/5">
+        <div className="font-sans ml-5 mb-2 mt-3 mr-2 text-lg">
           {description}
         </div>
       </div>
