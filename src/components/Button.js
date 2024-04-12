1b6fb92cdeb61b5ce2986bed8ea6e1de1f35ced5
Raidah Fairooz <rfair008@ucr.edu>
2024-04-12 01:30:28 -0700

    small bug added cursor pointer on hover

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 const Button = ({ text, background }) => {
   return (
     <div
-      className={`inline-block  text-3xl text-center rounded-full font-start py-3 px-8  text-white transition hover:scale-110 ${background}`}
+      className={`inline-block  text-3xl text-center rounded-full font-start py-3 px-8  text-white transition hover:cursor-pointer hover:scale-110 ${background}`}
     >
       {text}
     </div>
