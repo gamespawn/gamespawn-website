167f0877af9d69a7f59fa4adf6d378aaa1ef27c0
Raidah Fairooz <rfair008@ucr.edu>
2024-04-12 19:51:52 -0700

    small bugs

--- src/components/home/Project.js
+++ src/components/home/Project.js
@@ -3,19 +3,15 @@ import Button from "/src/components/Button.js";
 
 const Project = () => {
   return (
-    <div className="flex justify-center items-center w-full">
-      <div className="text-center w-6/12">
-        <div className="text-6xl font-russo mb-2">CHECK OUT OUR PROJECTS!</div>
-        <div className="grid grid-cols-2 my-8">
-          <div className="font-monda text-xl text-right leading-8 w-10/12">
-            At the Heart of the Forest
-          </div>
-          <div className="font-monda text-xl text-right leading-8 w-10/12">
-            Sketch
-          </div>
+    <div className="flex flex-col justify-center items-center w-full">
+      <div className="text-6xl font-russo mb-2">CHECK OUT OUR PROJECTS!</div>
+      <div className="grid grid-cols-2 my-8">
+        <div className="font-monda text-xl text-right leading-8">
+          At the Heart of the Forest
         </div>
-        <Button text="see all projects" background="bg-game-blue-300 p-10" />
+        <div className="font-monda text-xl text-right leading-8">Sketch</div>
       </div>
+      <Button text="see all projects" background="bg-game-blue-300 p-10" />
     </div>
   );
 };
