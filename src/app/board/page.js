da985db0fc8aa768b3c697022a0ccf652e5c239a
Atharva <147217989+atharvanev@users.noreply.github.com>
2024-04-15 00:32:14 -0700

    removed extra test components

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Team from "@/components/board/Team";
-import Banner from "/src/components/PageBanner.js";
 
 const page = () => {
   return (
     <div>
-      <Banner title={"MEET THE BOARD"} />
       <Team />
     </div>
   );
