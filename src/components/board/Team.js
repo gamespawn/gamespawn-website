ecb1c989f14f73844eafeae425e179da1ab4b907
Raidah Fairooz <rfair008@ucr.edu>
2024-05-03 18:12:17 -0700

    added another div to fix spacing

--- src/components/board/Team.js
+++ src/components/board/Team.js
@@ -4,18 +4,20 @@ import BOARD from "@/data/Board";
 
 const Team = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-stretch justify-items-center gap-y-4 gap-x-4 my-10">
-      {BOARD.map((person, index) => (
-        <Profile
-          key={index}
-          name={person.name}
-          email={person.email}
-          title={person.title}
-          image={person.image}
-          year={person.year}
-          major={person.major}
-        />
-      ))}
+    <div className="flex justify-center items-center my-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {BOARD.map((person, index) => (
+          <Profile
+            key={index}
+            name={person.name}
+            email={person.email}
+            title={person.title}
+            image={person.image}
+            year={person.year}
+            major={person.major}
+          />
+        ))}
+      </div>
     </div>
   );
 };
