ea09d1558b0b11bd0fda23b8d7d60bd0f34c7a37
sc <sau.mia206@icloud.com>
2024-05-13 17:58:15 -0700

    Created Card component for projects

--- src/components/projects/Card.js
+++ src/components/projects/Card.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ filler }) => {
+  return (
+    <div className="border-2 border-black w-72 rounded-3xl">
+      <Image
+        src={ image }
+        alt="picture of project"
+        className="mt-4 mr-auto ml-auto"
+      />
+
+      <div className="ml-3 mt-4">
+        <div className="font-russo font-bold text-xl">{filler}</div>
+
+      </div>
+    </div>
+  );
 };
 
 export default Card;
