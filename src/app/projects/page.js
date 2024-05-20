62db60fd3207ccea8755bc40c4bb64aef0676a04
Raidah Fairooz <rfair008@ucr.edu>
2024-05-20 00:18:41 -0700

    added suspense

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,12 +2,15 @@ import React from "react";
 import Intro from "@/components/projects/Intro";
 import Search from "@/components/projects/Search";
 import Cards from "@/components/projects/Cards";
+import { Suspense } from "react";
 const page = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <Intro />
+      <Suspense />
       <Search />
       <Cards />
+      <Suspense />
     </div>
   );
 };
