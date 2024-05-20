62d541eee43295b79375f1e736147b9803793adf
Atharva <147217989+atharvanev@users.noreply.github.com>
2024-05-19 22:22:52 -0700

    add banners to all pages'

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Intro from "@/components/projects/Intro";
+import Banner from "/src/components/PageBanner.js";
 
 const page = () => {
   return (
-    <div className="flex items-center justify-center">
-      <Intro />
+    <div>
+      <Banner title={"Projects"} />
+      <div className="flex items-center justify-center">
+        <Intro />
+      </div>
     </div>
   );
 };
