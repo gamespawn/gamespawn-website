8161564401508c67febefa277a1e53587cef69c6
Atharva <147217989+atharvanev@users.noreply.github.com>
2024-06-03 19:15:54 -0700

    removed rounded corners to match figma

--- src/components/projects/Checkbox.js
+++ src/components/projects/Checkbox.js
@@ -7,7 +7,7 @@ const Checkbox = ({ toggle, onClick = () => {}, text = "" }) => {
       onClick={onClick}
     >
       <div
-        className={`w-4 h-4 rounded-sm mr-2 border-2 border-black flex items-center justify-center`}
+        className={`w-4 h-4 mr-2 border-2 border-black flex items-center justify-center`}
       >
         <BsCheckLg
           className={`${toggle ? "text-black" : "text-white"} text-xl`}
